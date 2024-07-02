7c23d1dedca2b125d77334360292b680a1ef185d
Maximillian Leonard <maximillian1508@gmail.com>
2024-07-02 21:10:09 +0800

    feat/add identity framework & local db

--- DDAC_Project/Program.cs
+++ DDAC_Project/Program.cs
@@ -1,7 +1,18 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using DDAC_Project.Data;
+using DDAC_Project.Areas.Identity.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("DDAC_ProjectContextConnection") ?? throw new InvalidOperationException("Connection string 'DDAC_ProjectContextConnection' not found.");
+
+builder.Services.AddDbContext<DDAC_ProjectContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<DDAC_ProjectUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<DDAC_ProjectContext>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
@@ -13,11 +24,12 @@ if (!app.Environment.IsDevelopment())
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
 
 app.Run();
