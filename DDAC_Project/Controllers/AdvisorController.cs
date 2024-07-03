42b044ff1b5bbd2a90022ab9d9070aac54eef6c9
EVI RENALDI SOLY <TP063116@mail.apu.edu.my>
2024-07-03 22:32:20 +0800

    Feat/add advisor dashboard page

---
+++ DDAC_Project/Controllers/AdvisorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DDAC_Project.Controllers
+{
+    public class AdvisorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
