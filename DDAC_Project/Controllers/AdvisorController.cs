70b7bc17ac56cb2870fdfe58b8dae8468bb53567
EVI RENALDI SOLY <TP063116@mail.apu.edu.my>
2024-07-08 16:57:42 +0800

    Feat/add back-end for select user page

--- DDAC_Project/Controllers/AdvisorController.cs
+++ DDAC_Project/Controllers/AdvisorController.cs
@@ -1,10 +1,12 @@
 ﻿using DDAC_Project.Areas.Identity.Data;
 using DDAC_Project.Areas.Identity.Pages.Account.Manage;
 using DDAC_Project.Data;
+using DDAC_Project.Views.Advisor;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
 using System.Security.Claims;
 using System.Threading.Tasks;
 
@@ -26,9 +28,23 @@ namespace DDAC_Project.Controllers
             public int? ClientCount { get; set; }
             public int? CommentCount { get; set; }
             public decimal ManagedAssets { get; set; }
-            public List<DDAC_ProjectUser> AssignedClients { get; set; }
+            public List<UserModel> AssignedClients { get; set; }
             public List<ClientAssetInfo> ClientAssets { get; set; }
 
+        }
+
+        public class UserModel
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string ClientID { get; set; }
+            public string Email { get; set; }
+            public string PhoneNumber { get; set; }
+        }
+
+        public class SelectUserModel
+        {
+            public List<UserModel> Users { get; set; }
         }
 
         private List<int> GetClientIdsForAdvisor(int advisorId)
@@ -68,7 +84,14 @@ namespace DDAC_Project.Controllers
                 .Where(c => c.AdvisorId == advisorId)
                 .Select(c => new
                 {
-                    c.User,
+                    userModel = new UserModel
+                    {
+                        FirstName = c.User.FirstName,
+                        LastName = c.User.LastName,
+                        Email = c.User.Email,
+                        PhoneNumber = c.User.PhoneNumber,
+                        ClientID = c.ClientId.ToString()
+                    },
                     TotalAssets = _context.Transactions
                         .Where(t => t.ClientId == c.ClientId)
                         .Sum(t => t.Amount)
@@ -80,14 +103,14 @@ namespace DDAC_Project.Controllers
                 ClientCount = totalClient,
                 CommentCount = totalComment,
                 ManagedAssets = totalManagedAssets,
-                AssignedClients = assignedClients.Select(ac => ac.User).ToList(),
+                AssignedClients = assignedClients.Select(ac => ac.userModel).ToList(),
                 ClientAssets = assignedClients.Select(ac => new ClientAssetInfo
                 {
-                    Name = $"{ac.User.FirstName} {ac.User.LastName}",
+                    Name = $"{ac.userModel.FirstName} {ac.userModel.LastName}",
                     Assets = ac.TotalAssets
                 }).ToList()
             };
-            Console.WriteLine(viewModel.ClientCount);
+
             return View("Index", viewModel);
         }
 
@@ -97,9 +120,30 @@ namespace DDAC_Project.Controllers
             return View();
         }
 
-        public IActionResult SelectUser()
+        public async Task<IActionResult> SelectUser()
         {
-            return View();
+            var advisorId = Convert.ToInt32(HttpContext.Session.GetInt32("AdvisorId"));
+
+            if (advisorId == 0)
+            {
+                return Challenge();
+            }
+
+            List<UserModel> assignedClients = await _context.Clients
+                .Where(c => c.AdvisorId == advisorId)
+                .Select(c => new UserModel
+                {
+                    FirstName = c.User.FirstName,
+                    LastName = c.User.LastName,
+                    ClientID = c.ClientId.ToString(),
+                    Email = c.User.Email,
+                    PhoneNumber = c.User.PhoneNumber,
+                })
+                .ToListAsync();
+
+            return View(assignedClients);
         }
+
+
     }
 }
